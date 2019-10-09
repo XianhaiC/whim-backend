d667d1818b96fa1c32143c0d6f524b165c7422a1
Xianhai Cao <xianhaic@gmail.com>
2019-10-09 00:09:08 -0700

    Spark serializer includes account handle if it exists

--- app/serializers/spark_serializer.rb
+++ app/serializers/spark_serializer.rb
@@ -1,10 +1,18 @@
 class SparkSerializer < ActiveModel::Serializer
   attributes :id, :name, :account_id, :impulse_id, :session_token,
-    :email
+    :email, :handle
 
   def email
     if !object.account.nil?
       return object.account.email
+    else
+      return nil
+    end
+  end
+
+  def handle
+    if !object.account.nil?
+      return object.account.handle
     else
       return nil
     end
